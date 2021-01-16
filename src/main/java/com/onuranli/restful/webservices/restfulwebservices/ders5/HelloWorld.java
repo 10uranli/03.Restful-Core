4bd91ccb8ae1bf1b3db9433108ec7d639e6120c8
10uranli <onur.anli@bil.omu.edu.tr>
2021-01-16 17:44:35 +0300

    AcceptHeaderLocaleResolver kullanımı

--- src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders5/HelloWorld.java
+++ src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders5/HelloWorld.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,5 +21,9 @@ public class HelloWorld {
 		return messageSource.getMessage("greeting.message",null, locale);
 	}
 	
+	@RequestMapping(method = RequestMethod.GET, path = "/helloWorld2")
+	public String helloWorld2(){
+		return messageSource.getMessage("greeting.message",null, LocaleContextHolder.getLocale());
+	}
 	
 }
--- src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders5/RestfulWebServicesApplication.java
+++ src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders5/RestfulWebServicesApplication.java
@@ -5,8 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -16,16 +18,27 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+//	@Bean
+//	public LocaleResolver localeResolver(){
+//		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+//		localeResolver.setDefaultLocale(Locale.US);
+//		return localeResolver;
+//	}
+//	
+	//Bunu LocaleContextHolder.getLocale() ile kullanıyorum.Direkt localden 
+	//aldığından sürekle _en gelir.Tekrar headerdan okuyabilmesi icin AcceptHeader'liyi kullanıyoruz.
 	@Bean
-	public LocaleResolver localeResolver(){
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	public LocaleResolver localeResolver2(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
-	@Bean
-	public ResourceBundleMessageSource bundleMessageSource(){
-		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
-		bundleMessageSource.setBasename("messages");
-		return bundleMessageSource;
-	}
+	//spring.messages.basename=messages application propertiesde kullandım aşağıdakine gerek yok
+
+//	@Bean
+//	public ResourceBundleMessageSource bundleMessageSource(){
+//		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+//		bundleMessageSource.setBasename("messages");
+//		return bundleMessageSource;
+//	}
 }
