876d82f0fabdf422dc06e7e204cf53cfc9dd3797
10uranli <onur.anli@bil.omu.edu.tr>
2021-01-23 18:39:40 +0300

    versionlama diğer yöntemler eklendi

--- src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders7/VersionController.java
+++ src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders7/VersionController.java
@@ -12,8 +12,39 @@ public class VersionController {
 		return new PersonV1("Onur");
 	}
 	
-	@GetMapping(path = "v2/person")
+	@GetMapping(path = "/v2/person")
 	public PersonV2 personV2(){
+		return new PersonV2(new Name("Onur", "Anli"));
+	}
+	
+	
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1(){
+		return new PersonV1("Onur");
+	}
+	
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2(){
+		return new PersonV2(new Name("Onur", "Anli"));
+	}
+	
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1(){
+		return new PersonV1("Onur");
+	}
+	
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2(){
+		return new PersonV2(new Name("Onur", "Anli"));
+	}
+	
+	@GetMapping(value = "/person/produces", produces = "application/company.app-v1+json")
+	public PersonV1 producesV1(){
+		return new PersonV1("Onur");
+	}
+	
+	@GetMapping(value = "/person/produces", produces = "application/company.app-v2+json")
+	public PersonV2 producesV2(){
 		return new PersonV2(new Name("Onur", "Anli"));
 	}
 }
