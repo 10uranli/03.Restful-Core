0882cfd540c4550caf891996aaf358dab282107f
10uranli <onur.anli@bil.omu.edu.tr>
2021-01-23 23:18:36 +0300

    delete ve post jpa için eklendi

--- src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders8/InsuranceJPAResource.java
+++ src/main/java/com/onuranli/restful/webservices/restfulwebservices/ders8/InsuranceJPAResource.java
@@ -28,7 +28,7 @@ public class InsuranceJPAResource {
 
 	@Autowired
 	private InsuranceRepository insuranceRepository;
-	//GET /users
+
 	
 	@GetMapping("/Insures")
 	public List<InsuredBean> retrieveAllInsured(){
@@ -43,43 +43,19 @@ public class InsuranceJPAResource {
 			throw new InsureNotFoundException("id : " + id + " nolu sigortalı bulunamadı");
 		return findById;
 	}
-	//POST Created return : created uri 
-//	@PostMapping("/Insures")
-//	public void createInsured(@RequestBody InsuredBean insuredBean){
-//		insuranceDao.save(insuredBean); //normal save
-//		//uri ve response status dönmek için
-//	}
-//	
-//	@PostMapping("/Insures")
-//	public ResponseEntity<Object> createInsured(@Valid @RequestBody InsuredBean insuredBean){
-//		InsuredBean savedInsure = insuranceDao.save(insuredBean); //normal save
-//		//uri ve response status dönmek için
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedInsure.getId()).toUri();
-//		return ResponseEntity.created(uri).build();
-//	}
-//	
-//	@DeleteMapping("/Insures/{id}")
-//	public void deleteInsure(@PathVariable Integer id){
-//		InsuredBean insure = insuranceDao.deleteById(id);
-//		if(insure == null)
-//			throw new InsureNotFoundException("id-" + id);
-//	}
-//	
-//	//HATEOAS için eklendi
-//	@GetMapping("/InsuresHateoas/{id}")//Normal get operasyonu ile yazılabilirdi.
-//	public EntityModel<InsuredBean> retrieveInsuredWithHateoas(@PathVariable Integer id){
-//		InsuredBean findById = insuranceDao.findById(id);
-//		
-//		if(findById == null)
-//			throw new InsureNotFoundException("id : " + id + " nolu sigortalı bulunamadı");
-//		
-//		
-//		EntityModel<InsuredBean> resource = new EntityModel<InsuredBean>(findById);
-//		WebMvcLinkBuilder linkTo = 
-//				linkTo(methodOn(this.getClass()).retrieveAllInsured());//linkTo ile hangi 
-//		
-//		resource.add(linkTo.withRel("all-insures"));//withRel ile all linkine referans verilir
-//		//HATEOAS
-//		return resource;
-//	}
+	
+	@DeleteMapping("/Insures/{id}")
+	public void deleteInsure(@PathVariable Integer id){
+		insuranceRepository.deleteById(id);
+	}
+	
+
+	@PostMapping("/Insures")
+	public ResponseEntity<Object> createInsured(@Valid @RequestBody InsuredBean insuredBean){
+		InsuredBean savedInsure = insuranceRepository.save(insuredBean); //normal save
+		//uri ve response status dönmek için
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedInsure.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
